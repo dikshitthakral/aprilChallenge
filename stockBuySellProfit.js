2e7613849ffc44486abe0973997046ec06dacee4
dikshitthakral <dikshitthakral@gmail.com>
2020-05-06 23:41:21 +0530

    Code to but and sell stocks to maximize the profit

---
+++ stockBuySellProfit.js
@@ -0,0 +1,24 @@
+// Say you have an array prices for which the ith element is the price of a given stock on day i.
+
+// Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+// Input: [7,1,5,3,6,4]
+// Output: 7
+// Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+//              Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let profit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) {
+      profit = profit + (prices[i] - prices[i - 1]);
+    }
+  }
+  return profit;
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
